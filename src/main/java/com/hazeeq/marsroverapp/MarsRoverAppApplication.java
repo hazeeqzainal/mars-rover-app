c033faf2a67b19ebbafd1ea36711bd4fe7f1f2fa
Hazeeq <hazeeqzainal@gmail.com>
2023-09-25 03:27:06 +0800

    Base files

---
+++ src/main/java/com/hazeeq/marsroverapp/MarsRoverAppApplication.java
@@ -0,0 +1,13 @@
+package com.hazeeq.marsroverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverAppApplication.class, args);
+	}
+
+}
